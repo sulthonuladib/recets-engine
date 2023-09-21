e837c5f0930cb3a8c202d07a288d81e54361d69a
sulthonuladib <adibcyber4rt@gmail.com>
2023-09-22 05:10:35 +0700

    feat: save search result via KafkaConnect with MongoSinkConnector

--- src/price-search.ts
+++ src/price-search.ts
@@ -1,4 +1,6 @@
 import client from 'amqplib';
+import { Kafka } from 'kafkajs';
+
 import { Order, Orderbook } from "./types";
 import { Exchange } from './price-convert';
 
@@ -47,15 +49,38 @@ const consumerChannelName = "search-service";
 const connection = await client.connect("amqp://localhost:5672");
 const consumer = await connection.createChannel();
 
+const KAFKA_PRODUCER_CONFIG = {
+    clientId: 'search-service',
+    brokers: ['localhost:9094']
+}
+const kafka = new Kafka(KAFKA_PRODUCER_CONFIG);
+const producer = kafka.producer({
+    allowAutoTopicCreation: true,
+});
+await producer.connect();
+
 console.log("[SEARCH] Connected to AMQP");
 console.log("[SEARCH] Started");
 
-await consumer.consume(consumerChannelName, (message) => {
+await consumer.consume(consumerChannelName, async (message) => {
     if (!message) return;
     const { exchange, coin, orderbook } = JSON.parse(
         message.content.toString()
     ) as SearchPayload;
     consumer.ack(message);
     const result = searchService.execute(orderbook);
-    console.log(`[${exchange}]:[${coin}] `, result);
+
+    await producer.send({
+        topic: 'your-kafka-topic-name',
+        messages: [
+            {
+                value: JSON.stringify({
+                    // _id: `${exchange}-${coin}`,
+                    ...result,
+                    coin,
+                    exchange
+                })
+            }
+        ]
+    });
 });
